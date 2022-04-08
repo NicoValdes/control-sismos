2247cca048883e0bdb567ad6615665e6611f6623
nicolas valdes <nicolas.valdes@live.com>
2022-04-08 11:40:25 -0400

    Primer commit

---
+++ scraping.py
@@ -0,0 +1,15 @@
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from datetime import date
+from time import sleep
+import os
+
+
+
